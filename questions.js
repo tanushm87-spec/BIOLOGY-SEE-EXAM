85c44abd7f370b2b239d3254025f61125f2357b1
tanushm87-spec <tanushm87@gmail.com>
2026-05-04 21:29:48 +0530

    Refactor questions.js to include quiz questions
    Removed CSS styles and added question objects for a quiz.

---
+++ questions.js
@@ -0,0 +1,29 @@
+const questions = [
+  // === UNIT 1 ===
+  {
+    q: "The nose of the redesigned Shinkansen bullet train is inspired by",
+    options: ["Kingfisher beak", "Crane peak", "Owl Peak", "Herons peak"],
+    ans: 0
+  },
+  {
+    q: "Passive cooling in skyscrapers is inspired by",
+    options: ["Termite Mounds", "Earthwork Mounds", "Ant Mounts", "Spiro Mounds"],
+    ans: 0
+  },
+  // ... Add all other questions from Unit 1, 2, 3 here
+  // I'll give you a few more examples, then you can continue
+
+  {
+    q: "Composites are",
+    options: ["Two or more constituent materials", "Materials with similar chemical properties", "Materials with similar physical properties", "Only one material with different shapes, colours"],
+    ans: 0
+  },
+  {
+    q: "The first biodegradable leather is",
+    options: ["Desserto", "Adriano Di Marti", "Bionic", "Exhale"],
+    ans: 0
+  },
+  // Add all remaining questions similarly...
+];
+
+export default questions;
